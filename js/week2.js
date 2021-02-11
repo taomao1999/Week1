f793506a2d3016f727d6f97f1271366391cc17fa
taomao <kennymao@outlook.com>
2021-02-10 18:16:16 -0600

    bounce ball

---
+++ js/week2.js
@@ -0,0 +1,40 @@
+let ballX;
+let ballY;
+let bg;
+let ballXspeed = 10;
+let ballYspeed = 10;
+
+function preload(){
+bg = loadImage("images/bg.png");
+}
+function setup() {
+createCanvas (windowWidth,windowHeight);
+
+ballX = random(windowWidth);
+ballY = random(windowHeight);
+rectMode(CENTER);
+imageMode(CENTER);
+}
+function draw(){
+  background(80,80,180);
+  image(bg,windowWidth/2,windowHeight/2,windowWidth,windowHeight);
+  rect(mouseX,mouseY,20,70);
+strokeWeight(random(0,10))
+  stroke(random(255),random(255),random(255));
+  ellipse(ballX,ballY,50,50);
+  ballX = ballX + ballXspeed;
+  ballY = ballY + ballYspeed;
+  if (ballX >= windowWidth || ballX <= 0) {
+    ballXspeed = ballXspeed * -1;
+    }
+  if (ballY >= windowHeight || ballY <= 0) {
+    ballYspeed = ballYspeed * -1;
+  }
+    if(dist(mouseX,mouseY,ballX,ballY) < 50){
+    ballYspeed = ballYspeed * -1;
+    ballXspeed = ballXspeed * -1;
+  }
+
+
+
+}
